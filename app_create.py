1fbeb09c5127d73394867693a5865411d9146136
Ceefar <rob.farq@googlemail.com>
2022-06-21 11:58:02 +0100

    Diverging to todo focus initally, no more habit rn

--- app_create.py
+++ app_create.py
@@ -2,12 +2,17 @@
 # for web app and test components
 import streamlit as st
 import streamlit.components.v1 as stc # < unused at present
+# for db access
+import db_integration
 
 
 # temp globals for testing/setup
 actual_name = "Ceefar"
 user_tags = ["fitness - cardio","fitness - trim","fitness - bulk","conditions - adhd","conditions - anxiety","skills - mysql","skills - portfolio","skills - programming","software - streamlit","lifestyle - wellness"]
 
+# these will just be db names, so do check if none exist already then show default else show existing.
+# some kinda option for premade or like preset would be cool but not rn ffs lol
+tasks_lists = ["My List"]
 
 # layout - maybe temp since barely using tbf
 topper = st.container()
@@ -15,7 +20,7 @@ topper = st.container()
 
 # page functions
 
-def add_habit_to_db_v0():
+def add_todo_task_to_db():
     pass
 
 
@@ -24,73 +29,62 @@ def run():
     # header topper
     with topper:
 
-        st.title(f"St.Atomic - The Atomic Todo App")
-        st.subheader(f"Create A New Habit")
+        st.title(f"St.Atomic")
+        st.subheader(f"Add Tasks")
         st.write("---")
 
 
     with st.container():
 
-        st.subheader(f"So {actual_name} What Do You Want To Start Improving?")
+        st.subheader(f"So {actual_name} what are we planning?")
         col1A, _ = st.columns([4,1])
-        col1A.write("Choose **Habit** or **Preset Habit** for short habits to develop, or if you have a classic style todo task you want to remember, choose **Classic Todo**")
+        col1A.write("Information lorem")
         st.write("##")
         
-        task_type = st.selectbox(
-        "Habit Type",
-        ('Habit', 'Preset Habit', 'Classic Todo'))
-
-        st.write("##")
-
-        with st.form(key="habit_task_form_v0"):
-
-            if task_type == "Habit":
-                st.write("**Use This For Developing Atomic Habits - It's What St.Atomic Is Made For!**")
-                base_habit = st.text_input("What Habit Do You Want To Grow?")
-                st.write("give a basic example duh (maybe inside the box double duh")
-
-                st.write("---")
-
-                col1B,col3B = st.columns([2,3])
-                col1B.write("What Is The Task's Alignment?")
-                col3B.write("What Are Alignments?")
-
-                col1C,_,col3C = st.columns([1,1,3])
-                with col1C:
-                    habit_alignment = st.radio("Choose 1",
-                    ('Positive', 'Neutral', 'Negative'))
-                with col3C:
-                    if habit_alignment == "Positive":
-                        st.write(":white_check_mark: Positive habits are ones that you **want** to develop, these are things that will improve your life in some way or another, however small or big. Think things like 'Read More', 'Meditate', 'Practice Coding', 'Work Out'.")
-
-                st.write("---")
-
-                st.write("What Areas Does This Relate To?")
-                habit_tags = st.multiselect("Add Tags",user_tags)
-                st.write("You can update tags later blah... by adding tags St.Atomic can help you form connections between similar habits lorem")
-
-                st.write("---")
-
-                submit_habit_form = st.form_submit_button(label="Start This Habit")
 
 
 
-            elif task_type == "Classic Todo":
-                st.write("Use This For Longer, Classic Style Todo Tasks, For Atomic Habits Choose **Habit** Instead")
-                base_todo = st.text_area("Add Your Todo Task Here")
-                submit_habit_form = st.form_submit_button(label="Add To My Todo List")
+        with st.form(key="todo_task_creator"):
 
 
-            elif task_type == "Preset Habit":
-                st.write("Full Functionality Coming Soon - Watch This Space!")
-                base_preset = st.selectbox("Choose A Preset Type",("Work - Programmer", "Exercise - Bulk up", "Exercise - Lose Weight", "Conditions - Adhd", "Lifestyle - Inner Peace"))
-                
-                submit_habit_form = st.form_submit_button(label="Coming Soon")
+            st.write("**What Do You Want To Do?**")
+            todo_title = st.text_input("Enter Task Title", value="a basic example", key="td_title")
+
+            todo_detail = st.text_area("Enter Task Details", value="a more thorough example", key="td_detail")
+
+            st.write("---")
+
+            col1B,col3B = st.columns([2,3])
+            col1B.write("What Is The Task's Alignment?")
+            col3B.write("What Are Alignments?")
+
+            col1C,_,col3C = st.columns([1,1,3])
+            with col1C:
+                habit_alignment = st.radio("Choose 1",
+                ('Positive', 'Neutral', 'Negative'))
+            with col3C:
+                if habit_alignment == "Positive":
+                    st.write(":white_check_mark: Positive habits are ones that you **want** to develop, these are things that will improve your life in some way or another, however small or big. Think things like 'Read More', 'Meditate', 'Practice Coding', 'Work Out'.")
+
+            st.write("---")
+            st.write("**Optional Enhancements**")
+            st.write("Make getting stuff done easier by taking 30 seconds to quickly configure...") # not configure its not a techy app its a todo list ffs
+
+            st.write("What Areas Does This Relate To?")
+            habit_tags = st.multiselect("Add Tags",user_tags)
+            st.write("You can update tags later blah... by adding tags St.Atomic can help you form connections between similar habits lorem")
+
+            st.write("---")
+
+            base_preset = st.selectbox("Choose A Preset Type", ("Work - Programmer", "Exercise - Bulk up", "Exercise - Lose Weight", "Conditions - Adhd", "Lifestyle - Inner Peace"))
+            
+            submit_habit_form = st.form_submit_button(label="Coming Soon")
+
+            task_type = st.selectbox("Your Tasks Lists", ('Habit', 'Preset Habit', 'Classic Todo'))
 
 
             if submit_habit_form:
-                if task_type == "Habit":
-                    add_habit_to_db_v0(base_habit, habit_alignment, habit_tags)
+                add_todo_task_to_db()
 
 
 
--- db_integration.py
+++ db_integration.py
@@ -46,6 +46,7 @@ def close_connection():
 
 # ---- CORE DB INTERACTIONS ----
 
+
 def create_all_tasks_v0_db():
     pass
 
@@ -60,33 +61,10 @@ def create_habit_tags_v0_db():
     create_table_query = "CREATE TABLE IF NOT EXISTS habittags_v0 (habittagid INT AUTO_INCREMENT, habitid INT NOT NULL, tagid INT, PRIMARY KEY(habittagid), FOREIGN KEY (habitid) REFERENCES habits_v0(habitid), FOREIGN KEY (tagid) REFERENCES tags_v0(tagid))" 
 
 
+# N0TE PROBABLY ALSO NEED TAG TYPE TABLE - IF DO NEED IT EITHER WAY TBF CAN DO WITHOUT FOR NOW
 def create_tags_list_v0_db():
     """ stores user created tags, not 100% sure as to where fk should have gone, possibly here, also unsure on constaint since now what you can't delete a habit? or tag? or wut?! - need to test """
     create_table_query = "CREATE TABLE IF NOT EXISTS tags_v0 (tagid INT AUTO_INCREMENT, tag VARCHAR(255), tagtype VARCHAR(255), PRIMARY KEY(tagid))"
-
-# PROBABLY ALSO NEED TAG TYPE TABLE - IF DO NEED IT EITEHRWAY TBF CAN DO WITHOUT FOR NOW
-
-
-def add_habit_to_db_v0():
-    pass
-
-
-
-
-# ---- T0DO LIST ----
- 
-def create_todo_v0_db():
-    """ stores todo only, for now anyway, focusing this first since its waaaay easier """
-    create_table_query = "CREATE TABLE IF NOT EXISTS todo_v0 (todoid INT AUTO_INCREMENT, user_title VARCHAR(255), user_todo TEXT, todo_status ENUM('in_progress', 'completed', 'paused'), created TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (todoid))"                             
-
-
-def add_todo_v0_db(todo_title, todo_entry):
-    """ add todo v0, simple af but just starting simple af duh """
-    add_table_query = f"INSERT INTO todo_v0 (user_title, user_todo, todo_status) VALUES ('{todo_title}', '{todo_entry}', 1)"
-    add_to_db(add_table_query)                             
-
-# OBVS TAGS AND SHIT BUT JUST START SIMPLE DUHHHH!
-
 
 
 def get_base_habit_data_v0():
@@ -96,3 +74,96 @@ def get_base_habit_data_v0():
     habit_data = get_from_db(get_habit_data_query)
     #print(f"{habit_data = }")
     return(habit_data)
+
+
+def add_habit_to_db_v0():
+    pass
+
+
+# ---- T0DO LIST ----
+# each named list is a todo that can have tasks added to it
+
+# IMPORTANT! -> NEED A LIST OF ACTUAL T0DO LISTS AS A TABLE, SO CAN ADD TAGS N I GUESS LIKE STATS N SHIT
+
+# main task and subtask heirarchical, toggle task is one that *has* to be done repeatedly - has a deadline and a reset which will turn it back on. (sumnt like that anyway, also considered like a floating task but couldnt really solidify the concept)
+# critical, urgent/high, moderate, low/meh, none (toggle? toggle default anyway?)
+# either due date or due date time not both (due date could still theoretically have the time and just end at 11:59:59 but whatever can figure out later)
+# actually just going slightly excess on things to add that may be temp or atleast optional but is easier to judge if they exist first
+# this includes impact/benefits (what will you get out of this task)
+# NOT INCLUDED BUT REALLY COULD -> timeframe (is like due date but instead of a date its like a range right maybe test actually quantifiable - few days, 1 week, fortnight, etc, vs more abstract like rapid term, short term, medium term, long term...
+# note certain things may be more relavent for toggle tasks or even habits but thats why we do lots of experimenting at this stage
+# difficulty could be named better, would prefer to be more about how far its pushing you outside your existing comfort zone but can have that as the descript for now anyways
+# consider that some things are more valid for certain types of task or like habits almost right like alignment for example or again timeframe
+# todolistid is the name of the todo list (storing all in one now) to do list id is that todolist (make a table for this! as need to link them)
+def create_new_todo_list_db(username:str = "default"):
+    """ main list, stores todo items and subtasks, for now anyway """
+    final_table_name = convert_todo_list_name_to_table_name(username)
+    create_table_query = (f"CREATE TABLE IF NOT EXISTS {username}_todo (taskid INT AUTO_INCREMENT, todoListID INT, todoListName VARCHAR(255), taskTitle VARCHAR(255), taskDetail TEXT,\
+                            taskType ENUM('main_task', 'sub_task', 'toggle_task'), taskStatus ENUM('in_progress', 'completed', 'paused'),\
+                            taskUrgency ENUM('critical', 'urgent', 'moderate', 'low', 'none'),\
+                            taskImpact ENUM('massive', 'significant', 'limited', 'minor'),\
+                            taskDifficulty ENUM('complicated','complex','average','simple'),\
+                            taskAlignment ENUM('add_positive', 'neutral', 'remove_negative'),\
+                            isTimeSensitive BOOL, dueDate TIMESTAMP, dueDateTime TIMESTAMP, created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,\
+                            updated TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (taskid))")        
+    add_to_db(create_table_query)                      
+
+
+def create_todo_id_name_db():
+    pass
+###################################################
+# T0DO LIST ID -> T0DO LIST NAME RELATIONAL TABLE #
+###################################################
+
+
+# so for this now ig tag type will be stuff like UI or the page name in question or like the debug/fixme, etc
+def create_tags_list_db(username:str = "default"):
+    """ creates default table that stores default tags for tasks (not overarching todo lists) """
+    create_table_query = f"CREATE TABLE IF NOT EXISTS {username}_tags (tagid INT AUTO_INCREMENT, tag VARCHAR(255), tagtype VARCHAR(255), PRIMARY KEY(tagid))"
+    add_to_db(create_table_query) 
+
+
+def create_todo_tags_relational_db(username:str = "default"):
+    """ relational table links tags to todo tasks, assumption here is that one db for one user (is just testing ffs chill is fine) so is fine to have as one overarching table, obvs instead could have 1 long table and id for each todoList which would be like each table but is fine for now """
+    create_table_query = f"CREATE TABLE IF NOT EXISTS {username}_todotags (todotagid INT AUTO_INCREMENT, todolist VARCHAR(255), todoid INT NOT NULL, tagid INT, PRIMARY KEY(todotagid), FOREIGN KEY (todoid) REFERENCES {username}_todo(taskid), FOREIGN KEY (tagid) REFERENCES {username}_tags(tagid))" 
+    add_to_db(create_table_query) 
+
+
+def add_todo_task_to_db(todo_title, todo_entry):
+    """ add task... """
+    add_table_query = f"INSERT INTO"
+    add_to_db(add_table_query)                             
+
+
+def get_base_todo_data():
+    """ grabs just the data from the habits_v0 table """
+    #get_habit_data_query = "SELECT * FROM habits_v0 LIMIT 1"
+    get_habit_data_query = "SELECT * FROM habits_v0"
+    habit_data = get_from_db(get_habit_data_query)
+    #print(f"{habit_data = }")
+    return(habit_data)
+
+
+# ---- string manipulation ----
+
+def convert_todo_list_name_to_table_name(todo_title:str):
+    """ jsut converts spaces to underlines, for sure will be edge cases, using split will be better but is fine for now """
+    split_title = todo_title.replace(" ","_")
+    return(split_title)
+
+
+# ---- main ----
+
+def main():
+    username = "ceefar"
+    create_new_todo_list_db(username)
+    create_tags_list_db(username)
+    create_todo_tags_relational_db(username)
+
+
+
+
+# ---- driver ----
+
+if __name__ == "__main__":
+    main()
