65be565ba4c7ab5a7c27195b729a4ccc32e2c0a3
Ceefar <rob.farq@googlemail.com>
2022-06-26 08:16:13 +0100

    new child formatting + now radio not slider

--- pages/app_view.py
+++ pages/app_view.py
@@ -91,7 +91,7 @@ border-left:10px solid #484848; color:white; font-family: 'Roboto', sans-serif; 
 # N0TE THE BOX SHADOW LOOKS MUCH BETTER ON DARK THEME - SO IMPLEMENT DARK STYLE THEME AS THE PRESET PLS
 CHILD_HTML_TEMPLATE = """
 <div style="padding-left:15px; padding-top:10px; font-family: 'Roboto', sans-serif; font-weight:600; color:grey;">{}</div>
-<div style="width:90%; height:100%; margin:5px 20px 1px 1px; padding:1px 0px 35px 15px; position:relative; border-radius:5px;
+<div style="width:60%; height:100%; margin:5px 20px 1px 1px; padding:1px 0px 35px 10px; position:relative; border-radius:5px;
 border=5px solid; box-shadow:0 0 1px 1px #eee; background-color:#31333F; font-weight:300;
 border-left:10px solid #484848; color:white; font-family: 'Roboto', sans-serif; box-shadow: 8px 5px 5px 5px rgba(49,51,63,0.15);">
 <h2 style="color:#{}; font-weight:300; margin-bottom:0px;">{}</h2>
@@ -140,17 +140,19 @@ def run():
         basic_tasks_dict_as_list = db.view_tasks_basic(db_username, todolistid) #FIXME : OWN FUNCTION 
         
 
-        def check_for_status_toggle(taskid, currentvalue, previousvalue):
-            """ write me - note also surely is a better way to do this? """
-            # FML COMMENT THIS LIKE FUCK AS OTHERWISE YOU 100% WILL FORGET HOW IT WORKS
-            print(f"{taskid = }")
-            print(f"{currentvalue = }")
-            print(f"{previousvalue = }")
+        def check_for_status_toggle(taskid, previousvalue):
+            """ 
+            uses the taskid (which is the key of the widget), plus 2 predeclared session state vars, a changing *current* value and a fixed *previous* value
+            and compares them, as on first run through these vars will be the same (nothing has changed so previous and current match),
+            but when one is toggled, the true current (which is the state of the toggle with the same key as its taskid) will now be different from the previous value,
+            implemented this way as, well A im still getting to grips with session state, and B because the vars are declared in a loop, 
+            so before when one was toggled they would all toggle and become their opposites, this resolves that problem.
+            this function is called via callback, when the given widget is toggled, the args are passed on change.
+            """
+            # the session state is set by taskid for each toggle widget, but tho its a number must be access as a string
             session_string = str(taskid)
-            print(f"{st.session_state[session_string] = }")
             true_current = st.session_state[session_string]
-            #print(f"{st.session_state[subtaskd['taskID']] = }")
-
+            #print(f"{st.session_state[session_string] = }")
             if true_current != previousvalue:
                 #send the id to be updated
                 print(f"UPDATE {taskid}")
@@ -162,66 +164,49 @@ def run():
             st.write("---")
             st.write("##")
             st.write(f"##### Main Task {j+1}")
-            #st.markdown(f"{taskd}")
-
-            #eba538
-            #32CD32
 
             stc.html(PARENT_HTML_TEMPLATE.format(taskd["taskStatus"], taskd["title"], taskd["detail"], taskd["createdDate"], taskd["taskType"]), height=200)
             
             parents_id = taskd["taskID"]
-            #print(f"{parents_id = }")
 
             basic_subtasks_dict_as_list = db.view_tasks_basic(db_username, parents_id, "child")
-            _, subtaskcol2 = st.columns([1,6]) 
+            #_, subtaskcol2 = st.columns([1,6]) 
 
             # YOU DONT NEED TO USE COL HERE JUST USE THE WIDTH % AND MARGIN/PADDING THEN CAN USE COLS FOR THE SLIDER
             # OR IDK JUST FIGURE IT OUT, POSSIBLY COULD REVERT TO CHECKBOX
 
-            with subtaskcol2:
+            #with subtaskcol2:
                 #only_once = True
-                for i, subtaskd in enumerate(basic_subtasks_dict_as_list):
+            for i, subtaskd in enumerate(basic_subtasks_dict_as_list):
 
-                    actual_current_status = db.get_current_status(db_username, subtaskd["taskID"])
+                actual_current_status = db.get_current_status(db_username, subtaskd["taskID"])
+                
+                if subtaskd["taskID"] not in st.session_state:
+                    st.session_state[subtaskd["taskID"]] = actual_current_status
+
+                if subtaskd["taskStatus"] == "completed":
+                    stc.html(CHILD_HTML_TEMPLATE.format(subtaskd["taskStatus"], "32CD32", subtaskd["title"], subtaskd["detail"], subtaskd["createdDate"], subtaskd["taskType"]), height=160)
+                else:
+                    stc.html(CHILD_HTML_TEMPLATE.format(subtaskd["taskStatus"], "eba538", subtaskd["title"], subtaskd["detail"], subtaskd["createdDate"], subtaskd["taskType"]), height=160)
+                #stc.html(CHILD_HTML_TEMPLATE.format(subtaskd["taskStatus"], subtaskd["title"], subtaskd["detail"], subtaskd["createdDate"], subtaskd["taskType"]), height=160)
+                
+                actual_current_status = db.get_current_status(db_username, subtaskd["taskID"])
+                st.session_state[f'{subtaskd["taskID"]}PREV'] = actual_current_status
+                slidercol1, _ = st.columns([4,2])
+                # changed from slider, looks cleaner, plus more options for additions (extra user inputs) now too
+                with slidercol1:
                     
-                    if subtaskd["taskID"] not in st.session_state:
-                        st.session_state[subtaskd["taskID"]] = actual_current_status
-
-                    if subtaskd["taskStatus"] == "completed":
-                        stc.html(CHILD_HTML_TEMPLATE.format(subtaskd["taskStatus"], "32CD32", subtaskd["title"], subtaskd["detail"], subtaskd["createdDate"], subtaskd["taskType"]), height=160)
+                    #status_select = st.select_slider(f'Set Status', value=actual_current_status, options=['in_progress','completed'], key=subtaskd["taskID"], on_change=check_for_status_toggle, args=[subtaskd["taskID"], st.session_state[f'{subtaskd["taskID"]}PREV']])
+                    print(f"{actual_current_status = }")
+                    if actual_current_status == "completed":
+                        radioindexint = 1
                     else:
-                        stc.html(CHILD_HTML_TEMPLATE.format(subtaskd["taskStatus"], "eba538", subtaskd["title"], subtaskd["detail"], subtaskd["createdDate"], subtaskd["taskType"]), height=160)
-                    #stc.html(CHILD_HTML_TEMPLATE.format(subtaskd["taskStatus"], subtaskd["title"], subtaskd["detail"], subtaskd["createdDate"], subtaskd["taskType"]), height=160)
-                    
-                    actual_current_status = db.get_current_status(db_username, subtaskd["taskID"])
-                    st.session_state[f'{subtaskd["taskID"]}PREV'] = actual_current_status
-                    status_select = st.select_slider(f'Set Status', value=actual_current_status, options=['in_progress','completed'], key=subtaskd["taskID"], on_change=check_for_status_toggle, args=[subtaskd["taskID"], st.session_state[subtaskd["taskID"]], st.session_state[f'{subtaskd["taskID"]}PREV']])
-                    
-                    #if status_select and only_once:
-                    
-                    #if status_select:
-                    #    toggle_task_status(db_username, subtaskd["taskID"], status_select)
-                    
-                        #only_once = False
-                    st.write("---")
-                
-                #st.markdown("#### the CURRENT on load")
-                #for subtaskd in basic_subtasks_dict_as_list:
-                #    st.write(subtaskd["taskID"])
-                #    st.write(st.session_state[subtaskd["taskID"]])
-                #    st.write("-")
-                
-                #st.write("---")
-                #st.markdown("#### the value ON LOAD")
+                        radioindexint = 0
+                    st.radio("Set Status", index=radioindexint, options=['in_progress','completed'], key=subtaskd["taskID"], on_change=check_for_status_toggle, args=[subtaskd["taskID"], st.session_state[f'{subtaskd["taskID"]}PREV']])
+                st.write("---")
 
-                #for subtaskd in basic_subtasks_dict_as_list:
-                #    st.write(subtaskd["taskID"])
-                #    st.write(st.session_state[f'{subtaskd["taskID"]}PREV'])
-                #    st.write("-")
 
-            #st.write(basic_subtasks_dict_as_list)
-
-            #st.write("---")
+                    
 
         st.write("---")
 
